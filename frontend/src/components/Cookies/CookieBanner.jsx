bebef5bcfdeacffddf38e06c805eddfe66e1bcb5
andreipaven <gigiijvan@gmail.com>
2025-06-27 20:17:51 +0300

    update

--- frontend/src/components/Cookies/CookieBanner.jsx
+++ frontend/src/components/Cookies/CookieBanner.jsx
@@ -1,37 +1,56 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CookieConsent from "react-cookie-consent";
 import themeColors from "../../Theme/themeColors.jsx";
 import useResponsive from "../Hooks/UseResponsive.jsx";
+import "../Styles/Cookies.css";
 
 function CookieBanner() {
-  const { isMediumScreen } = useResponsive();
+  const { isMediumScreen, isSmallScreen } = useResponsive();
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) {
+    return null;
+  }
+
   return (
     <CookieConsent
       enableDeclineButton={true}
       buttonText={"ACCEPT"}
       declineButtonText={"REFUZA"}
+      containerClasses={"cookie-banner"}
       style={{
         backgroundColor: themeColors.palette.secondary.contrastText,
         width: isMediumScreen ? "100%" : "50%",
         borderRadius: isMediumScreen ? "0em" : "1em",
-        margin: isMediumScreen ? "0em " : "1em",
         display: "flex",
         alignItems: "center",
         maxWidth: "60em",
+        position: "fixed",
+        marginBottom: isSmallScreen ? "5.8em" : isMediumScreen ? 0 : "1em",
+        left: isMediumScreen ? 0 : "1em",
       }}
       buttonStyle={{
         backgroundColor: themeColors.palette.primary.contrastText,
-        fontSize: "1.5em",
-        borderRadius: "1em",
-        padding: ".5em",
+        fontSize: "1em",
+        borderRadius: "11em",
+        padding: ".5em 1em",
+        fontWeight: "bold",
       }}
       declineButtonStyle={{
         backgroundColor: themeColors.palette.secondary.contrastText,
         color: themeColors.palette.primary.contrastText,
         border: `2px solid ${themeColors.palette.primary.contrastText}`,
-        fontSize: "1.5em",
-        borderRadius: "1em",
-        padding: ".5em",
+        fontSize: "1em",
+        borderRadius: "12em",
+        padding: ".5em 1em",
+        fontWeight: "bold",
       }}
       contentStyle={{
         alignItems: "start",
@@ -39,7 +58,7 @@ function CookieBanner() {
     >
       Folosim cookie-uri pe site-ul nostru web pentru a vă oferi cea mai
       relevantă experiență, amintindu-vă preferințele și repetând vizitele. Dând
-      clic pe „Accept”, sunteți de acord cu utilizarea a TOATE cookie-urilor.
+      clic pe „ACCEPT”, sunteți de acord cu utilizarea a TOATE cookie-urilor.
     </CookieConsent>
   );
 }
